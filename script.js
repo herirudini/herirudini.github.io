389c68c9ee5d16a36b6ab3ef78aa8513a0abe5fd
herirudini <75158630+herirudini@users.noreply.github.com>
2021-07-05 12:42:55 +0700

    update content

--- script.js
+++ script.js
@@ -39,3 +39,39 @@ window.addEventListener('click', e => {
 		x.classList.remove('responsive');
 		}
 	})
+
+
+	var time = new Date().getHours();
+	var greetTime = "";
+
+	switch (time) {
+		case (time < 10):
+			greetTime = ["Good morning", "Selamat pagi", "Доброе утро", "صباح الخير"];
+			break;
+		case (time < 14):
+			greetTime = ["Good day", "Selamat siang", "Добрый день", "مساء الخير"];
+			break;
+		case (time < 16):
+			greetTime = ["Good afternoon", "Selamat sore", "Добрый день", "مساء الخير"];
+			break;
+		case (time < 21):
+			greetTime = ["Good evening", "Selamat malam", "Добрый вечер", "مساء الخير"];
+			break;
+		default:
+			greetTime = ["Good night", "Selamat malam", "Спокойной ночи", "تصبح على خير"];
+	}
+
+	var language = window.navigator.userLanguage || window.navigator.language;
+	if (language === "en" || language === "en-US") {
+		greetLang = greetTime[0];
+	} else if (language === "id" || language === "id-ID") {
+		greetLang = greetTime[1];
+	} else if (language === "ru" || language === "ru-RU") {
+		greetLang = greetTime[2];
+	} else if (language === "ar" || language === "ar-SA") {
+		greetLang = greetTime[3];
+	}
+
+	const greeting = greetLang;
+
+// alert(greeting)
